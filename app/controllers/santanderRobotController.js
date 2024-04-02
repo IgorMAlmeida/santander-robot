b03d631337aad6bfe418fe46ddeaf8d6cdc0d8cf
IgorMAlmeida <igor.almeida@credifranco.com.br>
2024-04-02 11:59:19 -0300

    changing to formdata data receiving

--- app/controllers/santanderRobotController.js
+++ app/controllers/santanderRobotController.js
@@ -3,35 +3,38 @@ import { loginSantander } from '../services/loginSantander.js';
 import { getOperationScreen } from '../services/getOperationScreen.js';
 
 const app = express();
+
 const retryTimes = 5;
 let retriedTimes = 0;
 
 export async function santanderRobot(req, res) {
   try {
-      const codProposalProposal = req.query.proposal.toString();
-      const username = req.query.username.toString();
-      const password = req.query.password.toString();
+    const codProposalProposal = req.body.propostaId;      
+    const username = 'EX114688';
+    const password = 'CF@5400';
 
-      const { page, browser } = await loginSantander(username, password);
-      const data = await getOperationScreen(page, 'andamento', codProposalProposal);
+    const { page, browser } = await loginSantander(username, password);
+    const data = await getOperationScreen(page, 'andamento', codProposalProposal);
 
-      if (!data.status) {
-        if(retriedTimes < retryTimes) {
-          retriedTimes++;
-          santanderRobot(req, res);
-        }
-        throw new Error(data.message);
+    if (!data.status) {
+      if(retriedTimes < retryTimes) {
+        retriedTimes++;
+        await santanderRobot(req, res);
+        return;
       }
-
-      const responseObj = {
-          erro: false,
-          dados: data.data
-      };
-      res.status(200).json(responseObj);
-
       await browser.close();
+      throw new Error(data.message);
+    }
+
+    const responseObj = {
+        erro: false,
+        dados: data.data
+    };
+
+    await browser.close();
+    return (responseObj);
+
   } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ status: false, mensagem: 'Internal Server Error' });
+    return ({ status: false, mensagem: 'Internal Server Error' });
   }
 }
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import multer from 'multer';
+const upload = multer();
 import { santanderRobot } from './controllers/santanderRobotController.js';
 
 const router = express.Router();
 
-router.get('/santanderRobot', santanderRobot);
+router.post('/santanderRobot', upload.none(), async (req, res) => {
+    try {
+        const response = await santanderRobot(req, res);
+
+        if (response.status) {
+            throw new Error(response.message);
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+
+        console.error('Erro ao processar a solicitação:', message);
+        res.status(500).json({ status: false, error: error.message });
+    }
+  });
 
 export default router;
