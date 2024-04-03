f4ce0be94c0d30f92f1d6ada6a3b933b046de432
IgorMAlmeida <igor.almeida@credifranco.com.br>
2024-04-03 10:30:33 -0300

    .

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import routes from './app/routes.js';
+import http from 'http';
+import https from 'https';
 
-const app = express(); 
+const app = express();
 
 app.use('/', routes);
 
-app.listen(3000, function() {
-    console.log('Running on port 3000.');
+const redirectToHttps = (req, res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+};
+
+http.createServer(redirectToHttps).listen(80);
+
+https.createServer({
+
+}, app).listen(80); // Aqui, ambos HTTP e HTTPS estão ouvindo na porta 80
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Running on port ${port}.`);
 });
