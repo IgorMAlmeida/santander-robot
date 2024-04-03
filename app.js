0ddce6758b412529de5a7f1895b5533aba7221a4
IgorMAlmeida <igor.almeida@credifranco.com.br>
2024-04-03 10:41:41 -0300

    .

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 import express from 'express';
 import routes from './app/routes.js';
-import http from 'http';
-import https from 'https';
 
-const app = express();
+const app = express(); 
 
 app.use('/', routes);
 
-// Redirecionamento HTTP para HTTPS
-const httpApp = (req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-};
-
-http.createServer(httpApp).listen(80);
-
-https.createServer({
-}, app).listen(443);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Running on port ${port}.`);
+app.listen(3000, function() {
+    console.log('Running on port 3000.');
 });
