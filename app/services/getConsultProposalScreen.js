51c9a017370fbaa7008dbf50c5fbcaebccccc7de
Carlos Henrique <carlos.santos@credifranco.com.br>
2024-05-22 20:30:55 -0300

    fix: resolve docker error

--- app/services/getConsultProposalScreen.js
+++ app/services/getConsultProposalScreen.js
@@ -5,11 +5,10 @@ import { scrappingOlaConsult } from "./olaScrappingService.js";
 export async function getConsultProposalScreen(page, codProposal) {
   
   try {
-    await sleep(500);
     await page.type('::-p-xpath(//*[@id="NumeroProposta"])', codProposal);
     await clickElementByXpath(page,'//*[@id="btnPesquisar"]');
 
-    await sleep(2000);
+    await sleep(2500);
     const messageError = await checkElementAndText(page, '//*[@id="divMensagemErro"]/ul/li');
     if(messageError.status && messageError.text === 'Nenhuma proposta encontrada para o(s) filtro(s) informado(s).') {
       throw new Error('Nenhuma proposta encontrada para o(s) filtro(s) informado(s).');
