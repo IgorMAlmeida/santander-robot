a03c36f00772888d05eb1521e5191c53a4192267
Carlos Henrique <carlos.santos@credifranco.com.br>
2025-04-23 16:04:35 -0300

    fix: puppeteer initializer

--- app/services/Approval/InitializePuppeteer.js
+++ app/services/Approval/InitializePuppeteer.js
@@ -1,11 +1,17 @@
-import puppeteer from "puppeteer";
+import {executablePath} from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
+
+puppeteer.use(StealthPlugin());
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
 export async function initialize() {
     const browser = await puppeteer.launch({
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        headless: false,
-        ignoreDefaultArgs: ["--disable-extensions", "--enable-automation"],
-        protocolTimeout: 15000,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: false,
+      ignoreDefaultArgs: ["--disable-extensions", "--enable-automation"],
+      executablePath: executablePath(),
     });
 
     const page = await browser.newPage();
